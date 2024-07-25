df9fdb880171e0435005b40f1dd87cf028d3a1f5
Pau Abella <pauabellamolina1@gmail.com>
2024-07-25 19:35:47 +0200

    wip testing again

--- app/(screens)/event/paymentModal/index.tsx
+++ app/(screens)/event/paymentModal/index.tsx
@@ -27,7 +27,7 @@ export default function PaymentModalScreen() {
           <FeatherIcon name="x" size={30} color={Colors[theme].text} />
         </Pressable>
         <iframe
-          allow="payment https://elteutikt.com/"
+          allow="payment https://sis.redsys.es"
           style={styles.iframe}
           srcDoc={`
             <html>
