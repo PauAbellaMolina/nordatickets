027a0077c1b62831f2f6e0251ce4db2543903620
Pau Abella <pauabellamolina1@gmail.com>
2024-12-02 03:07:20 +0100

    get event poster image by id and not slug

--- app/(screens)/event/[slug].tsx
+++ app/(screens)/event/[slug].tsx
@@ -60,7 +60,7 @@ export default function EventDetailScreen() {
 
   useEffect(() => {
     if (!event) return;
-    supabase.storage.from('event_posters').list('', { search: event.slug })
+    supabase.storage.from('event_posters').list('', { search: event.id.toString() })
     .then(({ data, error }) => {
       if (error || !data.length) {
         setEventPosterImageUrl(null);
